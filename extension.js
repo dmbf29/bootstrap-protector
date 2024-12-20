26ca0764730b881bfba0ee4c268b808ce1437161
Douglas Berkley <douglasmberkley@gmail.com>
2024-12-20 17:14:49 +0900

    extenstion recognizing one class and throwing an error

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+// Store the decoration type globally
+let decorationType = null;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +11,93 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "bootstrap-protector" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bootstrap-protector" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "bootstrap-protector.checkConflicts",
+    function () {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+      const document = editor.document;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bootstrap-protector.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      // Clear previous decorations
+      if (decorationType) {
+        editor.setDecorations(decorationType, []); // Clear existing decorations
+      }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Bootstrap Protector!');
-	});
+      if (document.languageId !== "scss") {
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      const bootstrapClasses = [];
+      // Display a message box to the user
+      // vscode.window.showWarningMessage("You are overriding Bootstrap a class!");
+      const content = document.getText();
+      const wordToSearch = "btn"; // Replace with the word you're searching for
+      const regex = new RegExp(`\\.\\b${wordToSearch}\\b(?=\\s|$|{)`, "g");
+      const matches = [...content.matchAll(regex)];
+
+      // if (matches.length > 0) {
+      //   vscode.window.showWarningMessage(
+      //     "You are overriding Bootstrap a class!"
+      //   );
+      // }
+      if (matches.length > 0) {
+        const decorations = [];
+
+        matches.forEach((match) => {
+          const startPos = document.positionAt(match.index);
+          const line = startPos.line;
+
+          // Create a decoration for the line
+          const decoration = {
+            range: new vscode.Range(line, 0, line, 0),
+            hoverMessage: `Overriding Bootstrap classes is not recommended.`,
+            renderOptions: {
+              after: {
+                contentText: `⚠️ Warning: "${wordToSearch}" is already being used by Bootstrap.`,
+                color: "red",
+                backgroundColor: "rgba(240, 132, 132, 0.2)",
+              },
+            },
+          };
+
+          decorations.push(decoration);
+        });
+
+        // Create a decoration type
+        decorationType = vscode.window.createTextEditorDecorationType({
+          isWholeLine: true,
+        });
+
+        // Apply decorations to the editor
+        editor.setDecorations(decorationType, decorations);
+
+        vscode.window.showInformationMessage(
+          `Found ${matches.length} occurrence(s) of "${wordToSearch}" in the current SCSS file.`
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+  if (decorationType) {
+    decorationType.dispose(); // Dispose of the decoration type on extension deactivation
+  }
+}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
